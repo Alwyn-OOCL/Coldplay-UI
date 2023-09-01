b67b794a4fccd946d08c074b9242fc973cd5979f
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-09-01 16:19:17 +0800

    feat, define url for qa

--- src/api/api.js
+++ src/api/api.js
@@ -5,10 +5,7 @@ const api = axios.create({
         baseURL: 'https://649138542f2c7ee6c2c7dd46.mockapi.io/api/v1/'
     },
     'qa': {
-        baseURL: 'https://days15-todo-backend-demo-qa.up.railway.app/'
-    },
-    'production': {
-        baseURL: 'https://days15-todo-backend-demo-production.up.railway.app/'
-    },
+        baseURL: 'https://days15-todo-backend-demo-production-30a0.up.railway.app/'
+    }
 }[process.env.REACT_APP_ENV || 'development']);
 export default api;
