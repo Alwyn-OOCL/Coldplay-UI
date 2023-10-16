9763f7e927b0431f72e6f5b2d56e73acdaa3ee07
weixiaobo88 <weixiaobo88@gmail.com>
2023-10-16 14:38:07 +0800

    Update api.js using env variable in different envs

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
+const MOCK_AIP_URL = "https://649138542f2c7ee6c2c7dd46.mockapi.io/api/v1/";
+const baseURLByEnv = process.env.REACT_APP_BASE_URL || MOCK_AIP_URL;
+
 const api = axios.create({
-    'development': {
-        baseURL: 'https://649138542f2c7ee6c2c7dd46.mockapi.io/api/v1/'
-    },
-    'qa': {
-        baseURL: 'https://days15-todo-backend-demo-production-30a0.up.railway.app/'
-    }
-}[process.env.REACT_APP_ENV || 'development']);
+  baseURL: baseURLByEnv,
+});
+
 export default api;
