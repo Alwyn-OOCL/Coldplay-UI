082711ea879fc32840f93603914d904ed1ff8041
HEAL <HEAL@OOCL.COM>
2024-12-07 15:09:19 +0800

    initialize project

--- src/App.js
+++ src/App.js
@@ -4,16 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <div className="nav-bar">
-        <nav>
-          <ul>
-            <li><NavLink to={'/'}>Home </NavLink></li>
-            <li><NavLink to={'/done'}>Done List </NavLink></li>
-            <li><NavLink to={'/help'}>Help </NavLink></li>
-          </ul>
-        </nav>
-      </div>
-      <Outlet></Outlet>
+      hello coldplay
     </div>
   );
 }
--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,9 @@
-import axios from "axios";
+// import axios from "axios";
 
-const MOCK_AIP_URL = "https://649138542f2c7ee6c2c7dd46.mockapi.io/api/v1/";
-const baseURLByEnv = process.env.REACT_APP_BASE_URL || MOCK_AIP_URL;
+// const MOCK_AIP_URL = "https://649138542f2c7ee6c2c7dd46.mockapi.io/api/v1/";
+// const baseURLByEnv = process.env.REACT_APP_BASE_URL || MOCK_AIP_URL;
 
-const api = axios.create({
-  baseURL: baseURLByEnv,
-});
+// export const api = axios.create({
+//   baseURL: baseURLByEnv,
+// });
 
-export default api;
