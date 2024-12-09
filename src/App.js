3a14677e08f316d60129b2f5a12024c3fd748556
Daniel Siu <daniel.siu@oocl.com>
2024-12-09 14:53:10 +0800

    feat: add base router in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./components/HomePage/HomePage";
 
 function App() {
   return (
-    <div>
-      hello coldplay
-    </div>
+    <Router>
+      <Header />
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
+
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">This is Header</div>
+    )
+    
+}
+
+export default Header;
