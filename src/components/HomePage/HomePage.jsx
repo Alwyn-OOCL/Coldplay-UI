bd1fb00854d1e31909a4f55773c94f1e5551fb52
ZHENGVI2 <Zhengvi2@oocl.com>
2024-12-09 20:26:59 +0800

    [SCRUM-1] feat, create home video component

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import HomePoster from '../HomePoster/HomePoster';
+import HomeVideo from '../HomeVideo/HomeVideo';
 
 const HomePage = () => {
   return (<div>
     <HomePoster/>
+    <HomeVideo/>
   </div>);
 };
 
---
+++ src/components/HomeVideo/HomeVideo.jsx
@@ -0,0 +1,84 @@
+import React, { useRef, useState } from 'react';
+import './HomeVideo.css';
+
+const HomeVideo = () => {
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(true); // 是否播放
+  const [isMuted, setIsMuted] = useState(true); // 初始为静音
+  const [volume, setVolume] = useState(0.5); // 初始化音量
+
+  const togglePlayPause = () => {
+    if (videoRef.current) {
+      if (isPlaying) {
+        videoRef.current.pause();
+      } else {
+        videoRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  const toggleMute = () => {
+    if (videoRef.current) {
+      videoRef.current.muted = !isMuted;
+      setIsMuted(!isMuted);
+    }
+  };
+
+  const handleVolumeChange = (event) => {
+    const newVolume = parseFloat(event.target.value);
+    setVolume(newVolume);
+    if (videoRef.current) {
+      videoRef.current.volume = newVolume;
+      if (newVolume > 0) {
+        videoRef.current.muted = false;
+        setIsMuted(false);
+      }
+    }
+  };
+
+  const toggleFullScreen = () => {
+    if (videoRef.current) {
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+      } else {
+        videoRef.current.requestFullscreen();
+      }
+    }
+  };
+
+  return (
+    <section className="homevideo">
+      <video
+        ref={videoRef}
+        className="homevideo-content"
+        autoPlay
+        muted={isMuted} // 初始静音
+        loop
+        onClick={togglePlayPause}
+      >
+        <source src={require('../../assets/videos/homevideo.mp4')} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="video-controls-left">
+        <button onClick={toggleMute}>
+          {isMuted ? 'Unmute' : 'Mute'}
+        </button>
+        <input
+          type="range"
+          min="0"
+          max="1"
+          step="0.1"
+          value={volume}
+          onChange={handleVolumeChange}
+        />
+      </div>
+      <div className="video-controls-right">
+        <button onClick={toggleFullScreen}>Fullscreen</button>
+      </div>
+    </section>
+  );
+};
+
+export default HomeVideo;
+
