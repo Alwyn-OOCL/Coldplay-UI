3928021f751ba0698a78e5b9a7bcb858b701cf2d
Daniel Siu <daniel.siu@oocl.com>
2024-12-10 14:54:49 +0800

    [SCRUM-4] feat: add concertDetailApi

---
+++ src/api/concertDetailApi.js
@@ -0,0 +1,11 @@
+import baseApi from './baseApi';
+
+export const concertDetail = async (id) => {
+    try {
+        const response = await baseApi.get(`/concertdetail/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error('Concert detail error:', error);
+        throw error;
+    }
+}
