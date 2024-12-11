511aae22b531104a1fa28b40ded50c72a92f2bcb
oocl-wonged7 <edmond.c.m.wong@oocl.com>
2024-12-11 17:28:11 +0800

    [SCRUM-42] feat: remove cosole.log

--- src/api/pages/myOrderApi.js
+++ src/api/pages/myOrderApi.js
@@ -3,7 +3,6 @@ import baseApi from "../baseApi";
 export const getOrders = async (userId) => {
     try {
       const response = await baseApi.get(`/orders?userId=${userId}`);
-      console.log(response);
       return response;
     } catch (error) {
       console.error("Error getting orders:", error);
