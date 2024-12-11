7c1decf8131389851d7649ee3df9b54c2247aa5a
Daniel Siu <daniel.siu@oocl.com>
2024-12-11 11:50:36 +0800

    [ALL] fix: remove duplicate footer

--- src/pages/ConcertListPage.js
+++ src/pages/ConcertListPage.js
@@ -104,7 +104,6 @@ export default function ConcertListPage() {
                     currentPage={currentPage}
                 />
             </main>
-            <Footer />
         </div>
     );
 }
