811009fb1e13818d1f9bf593741520fd731d1425
Daniel Siu <daniel.siu@oocl.com>
2024-12-12 18:28:50 +0800

    [SCRUM-36] feat: update charity details

--- src/data/charity.js
+++ src/data/charity.js
@@ -1,22 +1,22 @@
 export const charity = [
-    {
-      "charity_id": "1",
-      "charity_description": "Clean water for communities",
-      "img": "run.jpg"
-    },
-    {
-      "charity_id": "2",
-      "charity_description": "Education for underprivileged children",
-      "img": "book_.jpg"
-    },
-    {
-      "charity_id": "3",
-      "charity_description": "Medical aid to remote areas",
-      "img": "love.jpg"
-    },
-    {
-      "charity_id": "4",
-      "charity_description": "Environmental conservation",
-      "img": "sea.jpg"
-    }
-  ]
+  {
+    "charity_id": "1",
+    "charity_description": "Charity run event",
+    "img": "run.jpg"
+  },
+  {
+    "charity_id": "2",
+    "charity_description": "Book donation drive",
+    "img": "book_.jpg"
+  },
+  {
+    "charity_id": "3",
+    "charity_description": "Spreading love and care",
+    "img": "love.jpg"
+  },
+  {
+    "charity_id": "4",
+    "charity_description": "Ocean conservation efforts",
+    "img": "sea.jpg"
+  }
+];
